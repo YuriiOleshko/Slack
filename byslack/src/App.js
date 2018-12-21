c07cee41fdec0e2abd12570b2af427b51074f851
Yurii Oleshko <yurii.oleshko@gmail.com>
2018-12-21 13:17:15 +0200

    razmetka app

--- byslack/src/App.js
+++ byslack/src/App.js
@@ -1,11 +1,26 @@
 import React, { Component } from 'react';
+import ColorPanel from './ColorPanel/ColorPanel';
+import {Grid} from 'semantic-ui-react'
+import SidePanel from './SidePanel/SidePanel';
+import Messages from './Messages/Messages';
+import MetaPanel from './MetaPanel/MetaPanel';
+import "./App.css"
 
 class App extends Component {
   render() {
     return (
-      <div>
-        APp
-      </div>
+      
+        <Grid columns='equal' className='app'>
+        <ColorPanel/>
+        <SidePanel/>
+        <Grid.Column text textAlign='center'>
+        <Messages/>
+        </Grid.Column>
+        <Grid.Column width={4}>
+      <MetaPanel/>
+        </Grid.Column>
+     
+      </Grid>
     );
   }
 }
