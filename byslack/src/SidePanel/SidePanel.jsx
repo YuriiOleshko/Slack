0c8e41f3575e2bcc44a8ebf2a3002cfa9e8070eb
Yurii Oleshko <yurii.oleshko@gmail.com>
2018-12-26 15:06:06 +0200

    change color

--- byslack/src/SidePanel/SidePanel.jsx
+++ byslack/src/SidePanel/SidePanel.jsx
@@ -3,6 +3,7 @@ import { Menu } from 'semantic-ui-react';
 import UserPanel from '../UserPanel/UserPanel';
 import Channels from '../Channels/Channels';
 import DirectMessage from '../DirectMessage/DirectMessage';
+import {connect} from 'react-redux'
 
 class SidePanel extends Component {
     render() {
@@ -12,7 +13,7 @@ class SidePanel extends Component {
             inverted
             fixed='left'
             vertical
-            style={{background: '#4c3c4c', fontSize:'1.2rem'}}>
+            style={{background: this.props.primaryColor, fontSize:'1.2rem'}}>
                 <UserPanel/>
                 <Channels/>
                 <DirectMessage/>
@@ -20,5 +21,8 @@ class SidePanel extends Component {
         );
     }
 }
+const mapStateToProps=state=>({
+    primaryColor:state.colors.primary
+})
 
-export default SidePanel;
+export default  connect(mapStateToProps) (SidePanel);
